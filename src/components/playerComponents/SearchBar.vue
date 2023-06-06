86a74d70d28632526fd0c914becdcdb5c15d2e33
Kst0629 <116320487+Kst0629@users.noreply.github.com>
2023-06-06 21:33:25 +1200

    search bar
    search bar

--- src/components/playerComponents/SearchBar.vue
+++ src/components/playerComponents/SearchBar.vue
@@ -1,34 +1,78 @@
 <template>
-  <div class="search-bar">
-    <input type="text" v-model="query" @input="search"/>
-    <el-icon class="search-icon"><SearchBar /></el-icon>
+  <div class="main">
+    <el-form>
+      <el-form-item>
+        <el-input v-model="query" placeholder="Search Music" />
+      </el-form-item>
+    </el-form>
+  </div>
+  <Player :popShow="popShow" :list="data.List" :play-index="data.playIndex" :cancelClick="cancelClick"/>
+  <ul>
+    <li v-for="item in musicList" :key="item.musicName">
+      {{ item.musicName }}
+    </li>
+  </ul>
+  
+  <!-- <div class="search-bar">
+    <input type="text" v-model="query" @input="search"/> 
+    
+    <Player :popShow="popShow" :list="data.list" :play-index="data.playIndex" :cancelClick="cancelClick"></Player>
+
     <ul>
-      <li v-for="result in results" :key="result.id">
-        {{ result.data }}
+      <li v-for="item in musicList" :key="item.id">
+        {{ item.musicName }}
       </li>
     </ul>
-  </div>
+  </div> -->
+
 </template>
 
 <script setup>
-
+// import Table from './Table.vue';
+import Player from './Player.vue';
+import {getMusic} from '../../api/index';
+import {reactive,onMounted, ref, computed} from 'vue';
 import "../../styles/player.css";
 
-const SearchBar = {
-  name: "SearchBar",
-  data(){
-    return{
-      query: "",
-      results: [],
-      data: []
-    };
-  },
-  methods: {
-    search() {
-      this.results = this.data.filter((item) =>{
-        return item.name.toLowerCase().includes(this.query.toLowerCase());
-      });
-    },
-  },
-};
+const data = reactive({
+  list:[],
+  playIndex:0,
+  sideCategories: 'English'
+})
+
+const query = ref("");
+const musicList = computed(() => {
+  return data.list?.filter((item) =>{
+    return item.musicName.toLowerCase().indexOf(query.value.toLowerCase()) >=0;
+  })
+})
+
+onMounted(async () => {
+  try {
+    // Fetch music data using the getMusic function from the API
+    const musicData = await getMusic();
+    // Update the reactive data.list property with the fetched music data
+    data.list = musicData;
+  } catch (error) {
+    console.error('Error fetching music data:', error);
+  }
+});
+
+// const SearchBar = {
+//   name: "SearchBar",
+//   data(){
+//     return{
+//       query: "",
+//       results: [],
+//       data: []
+//     };
+//   },
+//   methods: {
+//     search() {
+//       this.results = this.data.filter((item) =>{
+//         return item.name.toLowerCase().includes(this.query.toLowerCase());
+//       });
+//     },
+//   },
+// };
 </script>
